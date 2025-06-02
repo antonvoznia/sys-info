3eaaafe8f2e5c836ce8591e0579c0ffc6e350c3a
Anton Voznia <antoncty@gmail.com>
2025-06-02 23:39:55 +0200

    Extended tests: Run 2 differente processes. Check closed process.

--- tests/program-tests.ps1
+++ tests/program-tests.ps1
@@ -27,21 +27,46 @@ function Write-TableToFile {
     return & "$ScriptPath" -OutputFule out.txt
 }
 
+# Tests implementation
+
 $CMDFullName = "cmd.exe"
 $CMDName = "cmd"
-function Start-CMDProcess {
-    $app = Start-Process $CMDFullName -PassThru
-    Write-Output "$processName started with PID $app.Id"
+
+$NotepadFullName = "Nodepad.exe"
+$NotepadName = "Notepad"
+function Start-Proc($Proc) {
+    $app = Start-Process $Proc -PassThru
+    Write-Output "$Proc started with PID $app.Id"
 
     return $app
 }
 
 function Test-NewProcessRun {
-    $appId = Start-CMDProcess
+    $appId = Start-Proc $CMDFullName
     $output = Get-ProcessTable | findstr $CMDName | findstr $appId.Id
     EvalTest $MyInvocation.MyCommand.Name ($output)
 }
 
+function Test-2DifferentProcessesRun {
+    $cmd = Start-Proc $CMDFullName
+    $notepad = Start-Proc $NotepadFullName
+    $outputCMD = Get-ProcessTable | findstr $CMDName | findstr $cmd.Id
+    $outputNotepad = Get-ProcessTable | findstr $NotepadName | findstr $notepad.Id
+    EvalTest $MyInvocation.MyCommand.Name ($outputCMD -and $outputNotepad)
+}
+
+function Test-RunProcessAndClose {
+    $cmd = Start-Proc $CMDFullName
+    $outputCMD1 = Get-ProcessTable | findstr $CMDName | findstr $cmd.Id
+    Stop-Process -Force -Id $cmd.Id
+    $outputCMD2 = Get-ProcessTable | findstr $CMDName | findstr $cmd.Id
+
+    EvalTest $MyInvocation.MyCommand.Name ($outputCMD1 -and  !$outputCMD2)
+}
+
+# Run test cases
 Test-NewProcessRun
+Test-2DifferentProcessesRun
+Test-RunProcessAndClose
 
 exit $TestResult
